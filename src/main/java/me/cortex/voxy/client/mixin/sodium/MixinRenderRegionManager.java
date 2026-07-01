b6c18536053414e3ef7818bce67ebc563da3f613
xiaoancute <xiaoan114514qwq@outlook.com>
2026-07-01 13:43:01 +0800

    Tolerate missing Sodium fade hook

--- src/main/java/me/cortex/voxy/client/mixin/sodium/MixinRenderRegionManager.java
+++ src/main/java/me/cortex/voxy/client/mixin/sodium/MixinRenderRegionManager.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = RenderRegionManager.class, remap = false)
 public class MixinRenderRegionManager {
-    @Redirect(method = "uploadResults(Lnet/caffeinemc/mods/sodium/client/gl/device/CommandList;Lnet/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegion;Ljava/util/Collection;)V", at = @At(value = "INVOKE", target = "Ljava/lang/Math;toIntExact(J)I"), remap = false)
+    @Redirect(method = "uploadResults(Lnet/caffeinemc/mods/sodium/client/gl/device/CommandList;Lnet/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegion;Ljava/util/Collection;)V", at = @At(value = "INVOKE", target = "Ljava/lang/Math;toIntExact(J)I"), remap = false, require = 0)
     private int voxy$cancelFade(long time) {
         var vrs = ((IGetVoxyRenderSystem)(Minecraft.getInstance().levelRenderer)).getVoxyRenderSystem();
         if (vrs!=null) {
